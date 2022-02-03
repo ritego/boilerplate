98debe09733d03232a5acf01f3e04007289cfa9f
Isaiah Iroko <isaiahiroko@gmail.com>
2022-02-03 08:23:47 +0100

    remove typeorm-model-generator

--- scripts/sync-to-staging.sh
+++ scripts/sync-to-staging.sh
@@ -1,3 +1,13 @@
+git checkout development
+git pull --rebase origin development
+
+git checkout staging
+git pull --rebase origin staging
+
+git checkout main
+git pull --rebase origin main
+git checkout development
+
 git checkout main
 git merge development
 git checkout development
